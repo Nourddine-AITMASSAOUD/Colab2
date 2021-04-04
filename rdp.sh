78dc91013c09631aa587cff15b822c409df430b4
Nourddine-AITMASSAOUD <61356188+Nourddine-AITMASSAOUD@users.noreply.github.com>
2021-04-04 15:14:55 +0100

    Create rdp.sh

---
+++ rdp.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
+{
+sudo useradd -m ALOKK
+sudo adduser ALOKK sudo
+echo 'ALOKK:8426' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install  --fix-broken
+sudo apt install nautilus nano 
+sudo apt  install obs-studio
+sudo apt  install firefox
+sudo adduser ALOKK chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - ALOKK -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is 8426 \n'
